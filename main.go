95942b457c0ce6bb5f1956ffcb6241fef653e646
KijijiKid <mateo-andre@gmx.de>
2026-06-30 14:44:43 +0200

    feat: used greetingPrefix

--- main.go
+++ main.go
@@ -14,22 +14,14 @@ const (
 )
 
 func Hello(name, lang string) string {
-	var prefix string
-	switch strings.ToLower(lang) {
-	case spanish:
-		prefix = spanishHelloPrefix
-	case french:
-		prefix = frenchHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
+	prefix := greetingPrefix(lang)
 	if name == "" {
 		name = "World"
 	}
 	return fmt.Sprintf("%s %s", prefix, name)
 }
 
-func GreetingPrefix(lang string) (prefix string) {
+func greetingPrefix(lang string) (prefix string) {
 	switch strings.ToLower(lang) {
 	case spanish:
 		prefix = spanishHelloPrefix
