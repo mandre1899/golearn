470faecfb6460a8906b8bd85241fb0ee734cb1e9
KijijiKid <mateo-andre@gmx.de>
2026-07-01 10:57:24 +0200

    feat: turned functions to methods

--- iteration/shapes.go
+++ iteration/shapes.go
@@ -1,15 +1,24 @@
 package iteration
 
+import "math"
+
+type Circle struct {
+	radius float64
+}
 
 type Rectangle struct {
 	width	float64
 	heigth	float64
 }
 
-func Perimeter(rec Rectangle) float64 {
-	return 2 * (rec.heigth + rec.width)
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.heigth + r.width)
 }
 
-func Area(rec Rectangle) float64 {
-	return rec.heigth * rec.width
+func (r *Rectangle) Area() float64 {
+	return r.heigth * r.width
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * (c.radius * c.radius)
 }
--- iteration/shapes_test.go
+++ iteration/shapes_test.go
@@ -1,10 +1,12 @@
 package iteration
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestPerimeter(t *testing.T) {
+func Test(t *testing.T) {
 	rec := Rectangle{10,10}
-	got := Perimeter(rec)
+	got := rec.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -13,11 +15,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rec := Rectangle{12,6}
-	got := Area(rec)
-	want := 72.0
+	t.Run("rectangle", func(t *testing.T) {
+		rec := Rectangle{12,6}
+		got := rec.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("Got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circle", func(t *testing.T) {
+		c := Circle{2}
+		got := c.Area()
+		want := 12.566370614359172
+
+		if got != want {
+			t.Errorf("Got %g want %g", got, want)
+		}
+	})
 }
